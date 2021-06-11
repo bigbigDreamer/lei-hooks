97af678a5efbf735002e62bb9bee632d1e02e2c7
eric_wang <jadeface.scholar@gmail.com>
2021-06-11 17:54:37 +0800

    feat: judge config

--- packages/hooks/webpack.config.js
+++ packages/hooks/webpack.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const { merge } = require('webpack-merge');
+const path = require('path');
+const baseConfig = require('../../webpack.base.config');
 
-}
+module.exports = merge(baseConfig, {
+    entry: './es/index.js',
+    output: {
+        filename: 'xi-hooks.js',
+        library: 'xi-hooks',
+        path: path.resolve(__dirname, './dist'),
+    },
+})
